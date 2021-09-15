d40b117cf6ce2b178dcb114af2cbf78ce1c35af2
retheshnair <retheshnair@users.noreply.github.com>
2021-09-15 07:50:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = terraform.workspace == "default" ? "us-east-1" : "us-west-2"
+  region = var.region
 }
 
 #Get Linux AMI ID using SSM Parameter endpoint in us-east-1
@@ -15,6 +15,6 @@ resource "aws_instance" "ec2-vm" {
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
   tags = {
-    Name = "${terraform.workspace}-ec2"
+    Name = "ec2-cli"
   }
 }
